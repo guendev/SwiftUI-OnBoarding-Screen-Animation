e2d3028849565d99255e88c7a7458c3b30a454ab
yuan <dnstylish@gmail.com>
2021-10-19 20:22:31 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yuan on 19/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/OnBoarding_Screen_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoarding_Screen_AnimationApp.swift
+//  Shared
+//
+//  Created by Yuan on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding_Screen_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
